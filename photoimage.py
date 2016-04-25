4ea3933192d71a055dfc07ee364a017ba73d91c6
mscai <939546871@qq.com>
2016-04-25 15:15:46 +0800

    modify photoimage.py and phtooimage3.py

--- photoimage.py
+++ photoimage.py
@@ -26,7 +26,7 @@ class SimpleImageBrower(Gui):
             if self.index<len(self.files):
                 self.show_image(self.files[self.index])
                 self.index+=1
-                print file
+                #print file
                 self.mainloop()
             else:
                 self.quit()
@@ -97,26 +97,93 @@ def simulate_button():
     t.pack()
     xin.mainloop()
 
-if __name__=="__main__":
+def create_login():
     def reg():
         s1=en1.get()
-        s2=en1.get()
+        s2=en2.get()
         t1=len(s1)
         t2=len(s2)
-
-
-
+        if s1=="111" and s2=="222":
+            la3["text"]="login succeed"
+        else:
+            la3["text"]="user or password wrong"
+            en1.delete(0,t1)
+            en2.delete(0,t2)
     xin=Tk()
-    la1=Label(xin,text="user").grid(row=0,sticky=W)
-    la2=Label(xin,text="password").grid(row=1,sticky=W)
-    la3=Label(xin).grid(row=3,sticky=W)
+    la1=Label(xin,text="user")
+    la1.grid(row=0,sticky=W)
+    la2=Label(xin,text="password")
+    la2.grid(row=1,sticky=W)
+    la3=Label(xin)
+    la3.grid(row=3,sticky=W)
 
-    en1=Entry(xin).grid(row=0,column=1,sticky=E)
-    en2=Entry(xin).grid(row=1,column=1,sticky=E)
-    en2["show"]="*"
+    en1=Entry(xin)
+    en1.grid(row=0,column=1,sticky=E)
+    en2=Entry(xin,show="*")
+    en2.grid(row=1,column=1,sticky=E)
 
-    bu=Button(xin,text="sign in").grid(row=2,column=1,sticky=E)
-
-
-
+    bu=Button(xin,text="sign in",command=reg)
+    bu.grid(row=2,column=1,sticky=E)
     xin.mainloop()
+
+def creat_menu():
+    root=Tk()
+    menubar=Menu(root)
+
+    fmenubar=Menu(menubar)
+    for item in ["new","open"]:
+        fmenubar.add_checkbutton(label=item)
+
+    fmenubar.add_separator()
+
+    for item in ["save","save as"]:
+        fmenubar.add_radiobutton(label=item)
+
+    emenubar=Menu(master=menubar)
+    for item in ["copy","past","cut"]:
+        emenubar.add_command(label=item)
+
+    vmenubar=Menu(menubar)
+    for item in ["default view","novel view"]:
+        vmenubar.add_command(label=item)
+
+    amenubar=Menu(menubar)
+    for item in ["version information","other message"]:
+        amenubar.add_command(label=item)
+
+    menubar.add_cascade(label="file",menu=fmenubar)
+    menubar.add_cascade(label="edit",menu=emenubar)
+    menubar.add_cascade(label="view",menu=vmenubar)
+    menubar.add_cascade(label="about",menu=amenubar)
+
+    root["menu"]=menubar
+
+    root.mainloop()
+
+def creat_popmenu():
+    def xin():
+        global root
+        Label(root,text="I love python").pack()
+
+    root=Tk()
+    menubar=Menu(root)
+
+    for x in ["vb","c","java","php"]:
+        menubar.add_command(label=x)
+
+    menubar.add_command(label="python",command=xin)
+
+    def pop(event):
+        menubar.post(event.x_root,event.y_root)
+
+    root.bind("<Button-3>",pop)
+    root.mainloop()
+
+
+
+
+
+#this section should be interpreted by Python 3.X
+from tkinter.dialog import *
+
+
